e1f3ceec7484f1b4aa66118f693f9ca4d29acb77
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-03 16:06:32 -0400

    Create Bird.java

---
+++ Flight/Bird.java
@@ -0,0 +1,27 @@
+package Flight;
+
+public class Bird implements Flight{
+    private String type;
+
+    public Bird(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public void fly(){
+        System.out.println("I'm a bird who flaps wings to fly");
+    }
+
+    @Override
+    public String toString(){
+        return "Bird: " + type;
+    }
+}
