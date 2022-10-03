6e483424bde576c2ce66f6713fad29312d402d92
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-03 16:05:50 -0400

    Update MyFarm.java

--- FarmAnimal/MyFarm.java
+++ FarmAnimal/MyFarm.java
@@ -5,13 +5,15 @@ public class MyFarm {
 
         MyFarm farm[] = new MyFarm[5];
 
-        farm[0] = new Duck ("Donald", "Male", 3.2, 5, "Quack Quack");
+        farm[0] = new Duck("Donald", "Male", 3.2, 5, "Quack Quack");
         farm[1] = new Duck("Cheese", "Female", 3.6, 5, "Quack Quack");
         farm[2] = new Cow("Molly", "Female", 1600.0, 3, "Moo Moo");
         farm[3] = new Chicken("Amelia", "Female", 1.8, 4, "Cluck Cluck");
         farm[4] = new Chicken("Dixie", "Female", 1.7, 4, "Cluck Cluck");
 
-        System.out.println(farm + feedLoadingSchedule());
+      System.out.println(farm.length);
+      farm.feedLoadingSchedule;
+
 
     }
 }
