56785de2da1bebbe1f9b88b47a351c667be8fdc6
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-03 16:07:23 -0400

    Update Flight.java

--- Flight/Flight.java
+++ Flight/Flight.java
@@ -2,78 +2,10 @@ package Flight;
 
 public interface Flight {
 
-    public class Airplane implements Flight{
-        private int year;
-        private String model;
-        private String modelNumber;
+    //Interface Flight: Contain only one void method fly() (no parameters).
 
-        public Airplane(int year, String model, String modelNumber) {
-            this.year = year;
-            this.model = model;
-            this.modelNumber = modelNumber;
-        }
-
-        public int getYear() {
-            return year;
-        }
-
-        public void setYear(int year) {
-            this.year = year;
-        }
-
-        public String getModel() {
-            return model;
-        }
-
-        public void setModel(String model) {
-            this.model = model;
-        }
-
-        public String getModelNumber() {
-            return modelNumber;
-        }
-
-        public void setModelNumber(String modelNumber) {
-            this.modelNumber = modelNumber;
-        }
-
-        @Override
-        public String flight(){
-         return ("I'm an airplane that relies on an engine to fly.");
-        }
-
-        public String toString(){
-            return "Airplane: " + "Model: " + model + "\n" +
-                    "Model Number: " + modelNumber + "\n" +
-                    "Year: " + year + "\n" +
-                    flight();
-        }
+    public abstract void fly();
 
 
-    }
 
-    public class Bird implements Flight{
-        private String type;
-
-        public Bird(String type) {
-            this.type = type;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        @Override
-        public String flight(){
-            return("I'm a bird who flaps wings to fly");
-        }
-        @Override
-        public String toString(){
-            return "Bird: " + type + " " + flight();
-        }
-    }
 }
