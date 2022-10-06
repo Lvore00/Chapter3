07afd253c96de74a5350ea000ba460eff1324472
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-05 22:17:51 -0400

    Update Bird.java

--- Flight/Bird.java
+++ Flight/Bird.java
@@ -21,6 +21,16 @@ public class Bird implements Flight{
     }
 
     @Override
+    public void walk(){
+        System.out.println("I walk on my feet.");
+    }
+
+    @Override
+    public void jump(){
+        System.out.println("I jump by leaping from my feet.");
+    }
+
+    @Override
     public String toString(){
         return "Bird: " + type;
     }
