57d0b6485d7f1a9c8a76f2d6307e3ac2a7036362
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-10-05 22:18:58 -0400

    Update ThingsThatFly.java

--- Flight/ThingsThatFly.java
+++ Flight/ThingsThatFly.java
@@ -1,25 +1,26 @@
 package Flight;
 
+import java.util.Arrays;
+
 public class ThingsThatFly {
 
     public static void main(String[] args) {
 
-        ThingsThatFly thingsThatFly = new ThingsThatFly[2];
-        
+        Flight[] thingsThatFly = new Flight[2];
+
         thingsThatFly[0] = new Airplane(2016, "Boeing 747");
         thingsThatFly[1] = new Bird("Eagle");
         thingsThatFly[2] = new Bird ("Hummingbird");
 
         System.out.println(thingsThatFly.length);
 
-        for (String item: thingsThatFly) {
-            fly(item, );
-            
-        }
+      for(int i = 0; i < thingsThatFly.length; i++){
 
-        public static void birdsMethod(ThingsThatFly thingsThatFly){
+          System.out.println(thingsThatFly[i].toString());
+          thingsThatFly[i].fly();
+      }
 
-        }
 
     }
 }
+
